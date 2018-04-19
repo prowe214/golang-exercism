e01bbef7a04213f807554c8090d43ccb47deacc3
Paul Rowe <prowe214@gmail.com>
2018-04-18 19:56:09 -0600

    hamming complete

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,26 @@
 // Package hamming calculates hamming distances
 package hamming
 
-// Distance takes two strands and returns distance
-func Distance(a, b string) (int, error) {
+import (
+	"fmt"
+)
 
+// Distance takes two strands and returns distance
+func Distance(a, b string) (result int, err error) {
+	distance := 0
+
+	// throw error if lengths are different
+	if len(a) != len(b) {
+		return -1, fmt.Errorf("Lengths cannot be different")
+	}
+
+	// iterate across both strings at same time,
+	// increment if chars don't match
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, err
 }
